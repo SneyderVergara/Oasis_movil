0193b0074fca22e49efcc16c9f7594d6633a33d1
manana <email@email.com>
2024-02-16 11:53:04 -0500

    16/Feb

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core'
 import { Router } from "@angular/router"; 
 import { exit } from "nativescript-exit";
+import { Page, TextField } from '@nativescript/core';
 
 @Component({
   selector: 'home',
   templateUrl: './home.html',
+  styleUrls: ['./home.css'],
 })
 export class HomeComponent {
-  public constructor(private router: Router) {
+  public constructor(private router: Router, private page: Page) {
     // Use the component constructor to inject providers.
+  }
+
+  ngOnInit(): void {
+    this.page.actionBarHidden = true;
   }
 
   public onExit(): void {
     exit(); // will close application
   }
 }
+
